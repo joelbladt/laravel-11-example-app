7b3e1a26dabd455bac1a3c9c9564cf076e06efc9
joelbladt <joel@bladt.de>
2024-08-25 00:58:11 +0200

    adjust the factory of LogMessage

--- database/factories/LogMessageFactory.php
+++ database/factories/LogMessageFactory.php
@@ -4,6 +4,8 @@ namespace Database\Factories;
 
 use App\Models\LogMessage;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Monolog\Level;
+use Psr\Log\LogLevel;
 
 class LogMessageFactory extends Factory
 {
@@ -11,11 +13,18 @@ class LogMessageFactory extends Factory
 
     public function definition(): array
     {
+        $randomLevelName = fake()->randomElement([
+            LogLevel::DEBUG, LogLevel::INFO, LogLevel::ERROR, LogLevel::ALERT,
+            LogLevel::NOTICE, LogLevel::WARNING, LogLevel::CRITICAL, LogLevel::EMERGENCY,
+        ]);
+
         return [
-            'level_name' => fake()->randomElement(['error', 'warning', 'info']),
-            'level' => fake()->randomElement(['200', '500', '503']),
+            'level_name' => $randomLevelName,
+            'level' => Level::fromName($randomLevelName),
             'message' => fake()->sentence(),
             'logged_at' => now(),
+            'context' => [],
+            'extra' => [],
         ];
     }
 }
